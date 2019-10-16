b8974b2e70e201dbfdb77530767ee4ddb2c60ce9
schan2023 <lamschan996@ufl.edu>
2019-10-16 12:04:59 -0400

    update collection id

--- server/controllers/habitController.js
+++ server/controllers/habitController.js
@@ -23,6 +23,34 @@ exports.create = function(req, res) {
     });
 };
 
-//Get habit by id
+//GET - Get habit by id
+exports.getHabitById = function(req, res) {
+    var habitId = req.params._id;
 
-//Update collection id of habit
+    Habit.findById(habitId, function(err, habit) {
+        if(err) {
+            res.status(400).json({error: 'Habit not found'});
+        }
+        else {
+            res.status(200).json({habit: habit});
+        }
+    });
+};
+
+//PUT - Update collection id of habit
+exports.updateCollectionId = function(req, res) {
+    let body = {
+        collectionId: req.body.collectionId
+    }
+    Habit.findByIdAndUpdate(req.params._id,
+      { collectionId: body.collectionId })
+     .then(habit => {
+          if(!habit) {
+              return res.status(400).json("error");
+          }
+          res.status(200).json({habit: habit});
+     })
+     .catch(err => {
+      res.status(400).send('error: ' + err)
+    })
+}
